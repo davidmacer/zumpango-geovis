ba5e3e2bb322a96877384414fd3ce3499a88f0b1
davidmacer <jdavid.macer@gmail.com>
2024-06-13 03:11:59 -0600

    Agrega polígonos de localidades

--- server.R
+++ server.R
@@ -8,8 +8,20 @@
 #
 
 library(shiny)
+library(sf)
+library(leaflet)
 
-# Define server logic required to draw a histogram
+# Importa la capa de localidades del gpkg de Zumpango
+zump_path_data <- "./data/zumpango.gpkg"
+zumpango_colonias <- st_read(zump_path_data,
+                             layer = "1.01 Colonias")
+zumpango_colonias <- st_transform(st_as_sf(zumpango_colonias), 4326)
+  
+zumpango_locs <- st_read(zump_path_data,
+                    layer = "1.03 Localidades")
+zumpango_locs <- st_transform(st_as_sf(zumpango_locs), 4326)
+
+# Define la lógica del servidor requerida para crear el mapa
 function(input, output, session) {
 
   output$mymap <- renderLeaflet({
@@ -17,7 +29,14 @@ function(input, output, session) {
     leaflet()  |>
       setView(-99.09917, 19.79694, 11) |>
       addProviderTiles("CartoDB.VoyagerLabelsUnder",
-                       options = providerTileOptions(noWrap = TRUE))
+                       options = providerTileOptions(noWrap = TRUE)) |>
+      addPolygons(data = zumpango_locs,
+                  color = "#b091d4",
+                  weight = 4)
+      # addPolygons(data = zumpango_colonias,
+      #             fill = "green",
+      #             weight = 4)
+      
   })
 
 }
