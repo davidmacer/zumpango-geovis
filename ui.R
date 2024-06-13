360fff00f8f9261c34ed13ef9c5bbb34d5594854
davidmacer <jdavid.macer@gmail.com>
2024-06-13 02:18:04 -0600

    Agrega sidebarpanel y mapa base

--- ui.R
+++ ui.R
@@ -8,26 +8,36 @@
 #
 
 library(shiny)
+library(leaflet)
 
 # Define UI for application that draws a histogram
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Proyecto Zumpango"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+      sidebarPanel(
+        id = "sidebar",
+        width = 2,
+        div(
+          style = 'font-family: Nunito; font-weight: 600;',
+          checkboxGroupInput(
+            "selectLayer",
+            h3("Selecciona la capa deseada"),
+            choices = list("A", "B", "C")
+            )
+          )
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+          width = 10,
+          div( # Mapa base que contiene las capas de Zumpango
+            leafletOutput(outputId = "mymap",
+                          height = 820)
+            )
+          )
+      )
 )
